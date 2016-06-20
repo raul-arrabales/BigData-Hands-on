2803a87669cf39e81c05dbf8c0fa739bd581e661
Raul Arrabales <raul.arrabales@gmail.com>
2016-06-20 15:29:33 +0200

    Spark-submit demo

---
+++ PiDemo.sh
@@ -0,0 +1,5 @@
+spark-submit 
+--class org.apache.spark.examples.SparkPi 
+--deploy-mode cluster 
+--master yarn 
+/usr/lib/spark/examples/lib/spark-examples-1.6.0-cdh5.7.0-hadoop2.6.0-cdh5.7.0.jar 10
