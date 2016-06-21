6e217018bdf8e2282bd1a1c24eab5c545d38372e
Raul Arrabales <raul.arrabales@gmail.com>
2016-06-21 11:42:00 +0200

    Added wc comments

--- quijote.scala
+++ quijote.scala
@@ -1,14 +1,26 @@
+/**
+ * Term Frequency example with El Quijote
+ * 
+ */
+
+// Read the text file (I didn't use a .txt extension when I put the file to HDFS)
 val textFile = sc.textFile("quijotedata")
 
+// Split lines by spaces
 val wordCounts = textFile.flatMap(line => line.split(“ “))
 
+// Filter out zero length segments
 val filterWords = wordCounts.filter(word => word.length() > 0)
 
+// Getting a map for the count of each word 
 val mapWords = filterWords.map(word => (word, 1))
 
+// Reduce the counts by word
 val countWords = mapWords.reduceByKey((a,b) => a + b )
 
+// Sort alphabetically by words
 val countWordsSort = countWords.sortByKey()
 
+// Save the TF table
 countWordsSort.saveAsTextFile(“wc-output”)
 
