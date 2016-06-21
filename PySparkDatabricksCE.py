2a26676dfa7b584b0a48b872ec7048c9b9bbe683
Raul Arrabales <raul.arrabales@gmail.com>
2016-06-21 14:27:25 +0200

    Update PySparkDatabricksCE.py

--- PySparkDatabricksCE.py
+++ PySparkDatabricksCE.py
@@ -1,3 +1,9 @@
 # On a Databricks CE notebook:
 
+# From dropbox: 
 rdd1 = sc.textFile("https://www.dropbox.com/s/6sajovchhfy3cun/quijote.txt?dl=0?raw=1")
+
+# From Tables in Databricks cloud:
+rdd1 = sc.textFile("/FileStore/tables/2ndixk251466511975346/quijote.txt")
+
+
