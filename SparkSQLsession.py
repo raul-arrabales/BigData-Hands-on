b6c2ceeeaf9fff39dbf13b07bef5dd598455a5e5
Raul Arrabales <raul.arrabales@gmail.com>
2016-06-24 23:04:01 +0200

    Update SparkSQLsession.py

--- SparkSQLsession.py
+++ SparkSQLsession.py
@@ -6,6 +6,12 @@ from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
 
 
+help(sqlContext)
+
+type(sqlContext)
+
+
+
 # In Databricks CE Cloud: 
 
 from pyspark.sql import Row
@@ -44,5 +50,56 @@ df6 = df1.join(df2, 'nombre', 'right_outer').select('nombre')
 print(df6.collect())
 
 
+# Generating Data:
 
+from faker import Factory
+fake = Factory.create()
+fake.seed(1234)
+
+# Each entry consists of last_name, first_name, ssn, job, and age (at least 1)
+from pyspark.sql import Row
+def fake_entry():
+  name = fake.name().split()
+  return Row(name[1], name[0], fake.ssn(), fake.job(), abs(2016 - fake.date_time().year) + 1)
+  
+# Create a helper function to call a function repeatedly
+def repeat(times, func, *args, **kwargs):
+    for _ in xrange(times):
+        yield func(*args, **kwargs)
+
+data = list(repeat(10000, fake_entry))
+
+data[0][0], data[0][1], data[0][2], data[0][3], data[0][4]
+
+len(data)
+
+help(sqlContext.createDataFrame)
+
+dataDF = sqlContext.createDataFrame(data, ('last_name', 'first_name', 'ssn', 'occupation', 'age'))
+
+dataDF.printSchema()
+
+sqlContext.registerDataFrameAsTable(dataDF, 'dataframe')
+
+help(dataDF)
+
+dataDF.rdd.getNumPartitions()
+
+newDF = dataDF.distinct().select('*')
+newDF.explain(True)
+
+dataDF.show(n=30, truncate=False)
+
+display(subDF)
+
+filteredDF = dataDF.filter(dataDF.age < 10)
+filteredDF.show(truncate=False)
+filteredDF.count()
+
+display(dataDF.orderBy('age').take(5))
+
+print dataDF.count()
+print dataDF.distinct().count()
+
+dataDF.groupBy('occupation').count().show(truncate=False)
 
