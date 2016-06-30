c0d1cfbdb0d036bcce41ad14583a618076f72d2e
Raul Arrabales <raul.arrabales@gmail.com>
2016-06-30 19:18:07 +0200

    Update StreamingSession.py

--- StreamingSession.py
+++ StreamingSession.py
@@ -1,3 +1,25 @@
+
+# Kafka install:
+# sudo yum clean all
+# sudo yum install kafka
+# sudo yum install kafka-server
+
+# Kafka setup
+# Edit /etc/kafka/conf/server.properties to ensure that the broker.id is unique for each node and broker in Kafka cluster, 
+# and zookeeper.connect points to same ZooKeeper for all nodes and brokers
+
+# Kafka start
+# sudo service kafka-server start
+
+# Zookeeper check:
+# zookeeper-client
+# ls /brokers/ids
+# get /brokers/ids/<ID>
+
+# Kafka settings:
+# kafka-topics --create --zookeeper quickstart.cloudera:2181 --topic wordcounttopic --partitions 1 --replication-factor 1
+
+
 import sys
 
 from pyspark import SparkContext, SparkConf
