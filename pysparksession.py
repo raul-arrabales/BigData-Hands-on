af95efab9db13a8fd4e7bb902da71b944708f3f1
Raul Arrabales <raul.arrabales@gmail.com>
2016-06-30 18:12:59 +0200

    comments clean-up

--- pysparksession.py
+++ pysparksession.py
@@ -2,79 +2,29 @@
 
 # coding: utf-8
 
-# In[1]:
+# Configuring pyspark in Jupyter
 
 import os
-
-
-# In[2]:
-
 import sys
 
-
-# In[3]:
-
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/jre"
-
-
-# In[4]:
-
 os.environ["SPARK_HOME"] = "/usr/lib/spark"
-
-
-# In[5]:
-
 os.environ["PYLIB"] = os.environ["SPARK_HOME"] + "/python/lib"
-
-
-# In[6]:
-
 os.environ["PYSPARK_PYTHON"] = "/home/cloudera/anaconda3/bin/python"
 
-
-# In[7]:
-
 sys.path.insert(0, os.environ["PYLIB"] +"/py4j-0.9-src.zip")
-
-
-# In[8]:
-
 sys.path.insert(0, os.environ["PYLIB"] +"/pyspark.zip")
 
-
-# In[9]:
-
 from pyspark import SparkConf
-
-
-# In[10]:
-
 from pyspark import SparkContext
 
-
-# In[11]:
-
 conf = SparkConf()
-
-
-# In[12]:
-
 conf.setMaster('yarn-client')
-
-
-# In[13]:
-
 conf.setAppName('anaconda-pyspark-language')
-
-
-# In[14]:
 
 sc = SparkContext(conf=conf)
 
-
-# In[15]:
-
-rdd = sc.textFile('/user/cloudera/quijotedata/quijote.txt')
+# rdd = sc.textFile('/user/cloudera/quijotedata/quijote.txt')
 
 
         
