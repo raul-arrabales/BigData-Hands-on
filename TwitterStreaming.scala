d538b1bbd5fadaff9cb961e574bdd52b06f0a049
Raul Arrabales <raul.arrabales@gmail.com>
2016-07-01 14:53:48 +0200

    Update TwitterStreaming.scala

--- TwitterStreaming.scala
+++ TwitterStreaming.scala
@@ -1,31 +1,43 @@
 
+/**
+ * Reading a stream from Twitter
+ * 
+ */
+ 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.streaming.twitter._
 
-val config = new SparkConf().setAppName("twitter-stream-sentiment")
-
+val config = new SparkConf().setAppName("twitter-stream")
 val sc = new SparkContext(config)
+
+# In DC Cloud sc is already created:
+# sc.getConf.setAppName("twitter-stream")
+
 sc.setLogLevel("WARN")
  
+# Setting batched of 5 seconds
 val ssc = new StreamingContext(sc, Seconds(5))
  
+# Fill in your API keys
 System.setProperty("twitter4j.oauth.consumerKey", "consumerKey")
 System.setProperty("twitter4j.oauth.consumerSecret", "consumerSecret")
-System.setProperty("twitter4j.oauth.accessToken", accessToken)
+System.setProperty("twitter4j.oauth.accessToken", "accessToken")
 System.setProperty("twitter4j.oauth.accessTokenSecret", "accessTokenSecret")
  
 val stream = TwitterUtils.createStream(ssc, None)
 
-val tags = stream.flatMap { status => status.getHashtagEntities.map(_.getText)
-}
+val statuses = stream.map(status => status.getText())
 
-tags
-    .countByValue()
-    .foreachRDD { rdd =>
-        
-        val now = org.joda.time.DateTime.now()
-        rdd
-          .sortBy(_._2)
-          .map(x => (x, now))
-          .saveAsTextFile(s"~/twitter/$now")
-      }
+val filtered = statuses.filter(status => status contains "big data")
+
+statuses.print()
+
+ssc.start()
+ssc.awaitTermination()
+ 
