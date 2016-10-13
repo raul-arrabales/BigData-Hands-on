136439af85f37ef825719b1391afd5cca1f196d5
Raul Arrabales <raul.arrabales@gmail.com>
2016-10-13 17:09:04 +0200

    Update PySparkDatabricksCE.py
    One line Quijote wc added

--- PySparkDatabricksCE.py
+++ PySparkDatabricksCE.py
@@ -6,4 +6,11 @@ rdd1 = sc.textFile("https://www.dropbox.com/s/6sajovchhfy3cun/quijote.txt?dl=0?r
 # From Tables in Databricks cloud:
 rdd1 = sc.textFile("/FileStore/tables/2ndixk251466511975346/quijote.txt")
 
+wordcounts = rdd1.map( lambda x: x.replace(',',' ').replace('.',' ').replace('-',' ').lower()) \
+        .flatMap(lambda x: x.split()) \
+        .map(lambda x: (x, 1)) \
+        .reduceByKey(lambda x,y:x+y) \
+        .map(lambda x:(x[1],x[0])) \
+        .sortByKey(False)
 
+wordcounts.take(10)
