97af85a0530c458d16a6978a5da0b0eb48b980b2
Raul Arrabales <raul.arrabales@gmail.com>
2016-10-21 21:51:37 +0200

    Update NLTKonSpark.py

--- NLTKonSpark.py
+++ NLTKonSpark.py
@@ -1,3 +1,10 @@
 # Assuming DatabricksCE Cloud Environment
 
 import nltk 
+
+# El Quijote completo en Español:
+# http://www.gutenberg.org/cache/epub/2000/pg2000.txt 
+# http://www.gutenberg.org/files/2000/2000-8.txt 
+
+
+
