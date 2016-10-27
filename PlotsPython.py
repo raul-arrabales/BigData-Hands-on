ddb92232fbdbb915d9137f64c6d54014270c099d
Raul Arrabales <raul.arrabales@gmail.com>
2016-10-27 02:26:25 +0200

    Update PlotsPython.py

--- PlotsPython.py
+++ PlotsPython.py
@@ -21,3 +21,52 @@ plt.yticks(())
 
 plt.show()
 
+#
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = Axes3D(fig)
+X = np.arange(-4, 4, 0.25)
+Y = np.arange(-4, 4, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X ** 2 + Y ** 2)
+Z = np.sin(R)
+
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.hot)
+ax.contourf(X, Y, Z, zdir='z', offset=-2, cmap=plt.cm.hot)
+ax.set_zlim(-2, 2)
+
+plt.show()
+
+#
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x,y):
+    return (1 - x / 2 + x**5 + y**3) * np.exp(-x**2 -y**2)
+
+n = 256
+x = np.linspace(-3, 3, n)
+y = np.linspace(-3, 3, n)
+X,Y = np.meshgrid(x, y)
+
+plt.axes([0.025, 0.025, 0.95, 0.95])
+
+plt.contourf(X, Y, f(X, Y), 8, alpha=.75, cmap=plt.cm.hot)
+C = plt.contour(X, Y, f(X, Y), 8, colors='black', linewidth=.5)
+plt.clabel(C, inline=1, fontsize=10)
+
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+#
+#
+
+
