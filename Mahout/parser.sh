60502ffe8de011589084e16ca6e78c509266f616
Raul Arrabales <raul.arrabales@gmail.com>
2017-02-15 23:00:31 +0100

    Update parser.sh

--- Mahout/parser.sh
+++ Mahout/parser.sh
@@ -1,3 +1,10 @@
+# Turn each movie into a different file
 while IFS=' ' read id title genres;
-  do echo "$genres" >> $2/$id;
+  do echo "$genres" >> "$2/$id";
 done < $1
+
+# Then generate files with this script:
+# mkdir files
+# chmod 755 parser.sh
+# ./parser.sh movies.dat files
+
