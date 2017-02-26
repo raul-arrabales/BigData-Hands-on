4b23d831445174fc4eb518848230c4ea887ba98f
Raul Arrabales <raul.arrabales@gmail.com>
2017-02-26 20:47:20 +0100

    Update ratings.py

--- Spark/ratings.py
+++ Spark/ratings.py
@@ -34,7 +34,42 @@ results = (
   .map(lambda (mId, (t, r)) : (t, r))
 )
 
-results.takeOrdered(10, key=lambda (t, r): ‐r)
+results.takeOrdered(10, key=lambda (t, r): -r)
 
 # Step by step explanation
+
+MovieRatingsRDD = sc.textFile('/FileStore/tables/p1ab1wsy1488114436633/ratings_small.dat').map(lambda l : l.split('::')).map(lambda (uId, mId, r, ts) : (int(mId), float(r)))
+MovieRatingsRDD.take(5)
+# [(122, 5.0), (185, 5.0), (231, 5.0), (292, 5.0), (316, 5.0)] 
+
+RatingsPerMovieRDD = MovieRatingsRDD.groupByKey()
+RatingsPerMovieRDD.take(5)
+# [(40962, <pyspark.resultiterable.ResultIterable at 0x7f6ef87a0650>),
+# (3, <pyspark.resultiterable.ResultIterable at 0x7f6ef87a0fd0>),
+# (8196, <pyspark.resultiterable.ResultIterable at 0x7f6ef87a0f10>),
+# (6, <pyspark.resultiterable.ResultIterable at 0x7f6ef87a0dd0>),
+# (8199, <pyspark.resultiterable.ResultIterable at 0x7f6ef87a0a50>)]
+
+AvgRatingsRDD = RatingsPerMovieRDD.map(lambda (mId, rs) : (mId, sum(rs) / len(rs)))
+AvgRatingsRDD.take(5)
+# [(40962, 2.5760869565217392),
+#  (3, 3.1605526441162457),
+#  (8196, 2.877551020408163),
+#  (6, 3.81640625),
+#  (8199, 3.9047619047619047)]
+
+movies.join(AvgRatingsRDD).map(lambda (mId, (t, r)) : (t, r)).take(5)
+# [(u'Brothers (Br\xf8dre) (2004)', 3.515151515151515),
+#  (u"Avventura, L' (a.k.a. Adventure, The) (1960)", 3.934065934065934),
+#  (u'Father of the Bride Part II (1995)', 3.0775154004106775),
+#  (u'GoldenEye (1995)', 3.427753303964758),
+#  (u'Cutthroat Island (1995)', 2.6911177644710578)]
+
+TitlesRatings = movies.join(AvgRatingsRDD).map(lambda (mId, (t, r)) : (t, r)) 
+TitlesRatings.takeOrdered(5, key=lambda (t, r): -r)
+# [(u'Life of Oharu, The (Saikaku ichidai onna) (1952)', 5.0),
+#  (u'Titicut Follies (1967)', 5.0),
+#  (u"Money (Argent, L') (1983)", 5.0),
+#  (u"Pervert's Guide to Cinema, The (2006)", 5.0),
+#  (u'Shanghai Express (1932)', 5.0)]
 
