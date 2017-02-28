a1dfa777127e5cf848e2abe113a94b6e02c249c6
Raul Arrabales <raul.arrabales@gmail.com>
2017-02-28 18:10:42 +0100

    Update ratings.py

--- Spark/SparkSQL/ratings.py
+++ Spark/SparkSQL/ratings.py
@@ -43,3 +43,26 @@ avgRs = sqlContext.sql("SELECT t, AVG(r) AS avgR " +
   "GROUP BY t ORDER BY avgR DESC")
 
 avgRs.take(10)
+
+# ------------------------------------------------
+# We can also count the number of ratings per user:
+
+ratings = (
+  sc.textFile('/FileStore/tables/p1ab1wsy1488114436633/ratings_small.dat')
+  .map(lambda l : l.split('::'))
+  .map(lambda (uId, mId, r, ts) : Row(uId=int(uId), mId=int(mId), r=float(r))))
+
+sqlContext.createDataFrame(ratings).registerTempTable("ratings") 
+
+UsrRatingsCount = sqlContext.sql("SELECT uId, COUNT(*) FROM ratings GROUP BY uId") 
+
+UsrRatingsCount.take(10)
+
+UsrRatingsCount.takeOrdered(10, key=lambda (t, r): -r)
+
+UsrRatingsCount = sqlContext.sql("SELECT uId, COUNT(*) FROM ratings GROUP BY uId ORDER BY COUNT(*) DESC")
+
+UsrRatingsCount.take(10)
+
+
+
