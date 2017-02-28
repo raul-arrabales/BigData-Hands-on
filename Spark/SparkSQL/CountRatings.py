d1a6a80cb9c040e5713b9e885e33086421493782
Raul Arrabales <raul.arrabales@gmail.com>
2017-02-28 18:13:30 +0100

    Update CountRatings.py

--- Spark/SparkSQL/CountRatings.py
+++ Spark/SparkSQL/CountRatings.py
@@ -22,21 +22,20 @@ from pyspark.sql import SQLContext, Row
 # sqlContext = SQLContext(sc)
 sqlContext = SQLContext.getOrCreate(sc.getOrCreate()) 
 
-
-
+# Load ratings file with rows uId, mId, r
 ratings = (
   sc.textFile('/FileStore/tables/p1ab1wsy1488114436633/ratings_small.dat')
   .map(lambda l : l.split('::'))
   .map(lambda (uId, mId, r, ts) : Row(uId=int(uId), mId=int(mId), r=float(r))))
 
+# Declare ratings as table
 sqlContext.createDataFrame(ratings).registerTempTable("ratings") 
 
+# Query number of ratings per user
 UsrRatingsCount = sqlContext.sql("SELECT uId, COUNT(*) FROM ratings GROUP BY uId") 
-
 UsrRatingsCount.take(10)
 
-UsrRatingsCount.takeOrdered(10, key=lambda (t, r): -r)
-
+# Query number of ratings per user, ordered
 UsrRatingsCount = sqlContext.sql("SELECT uId, COUNT(*) FROM ratings GROUP BY uId ORDER BY COUNT(*) DESC")
-
 UsrRatingsCount.take(10)
+
