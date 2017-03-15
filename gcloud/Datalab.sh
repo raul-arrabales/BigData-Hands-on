887696b6608552c762b0230dbddfecfe36b165c6
Raul Arrabales <raul.arrabales@gmail.com>
2017-03-16 00:14:24 +0100

    Update Datalab.sh

--- gcloud/Datalab.sh
+++ gcloud/Datalab.sh
@@ -1,7 +1,17 @@
+# Select the project from the list
 gcloud projects list 
 
+# Using the project Id
 gcloud config set core/project scaleml-161622
 
+# Set zone
 gcloud config set compute/zone europe-west1-b
 
-datalab create datalabML
+# Create datalab instance
+datalab create datalabml1
+
+# If connection loss
+atalab connect datalabml1
+
+# To delete the instance
+datalab delete datalabml1
