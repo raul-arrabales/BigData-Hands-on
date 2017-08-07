7649318accd84b7c7bbd11b4afb8e6de20bc9af3
Raul Arrabales <raul.arrabales@gmail.com>
2017-08-07 11:32:36 +0200

    Update Session1.py

--- Spark/Databricks/Session1.py
+++ Spark/Databricks/Session1.py
@@ -12,6 +12,29 @@ print(moviesRDD.take(4))
 print('--- Ratings:')
 print(ratingsRDD.take(4))
 
+# Workign with ratings, step by step
+
+# Current data format in the RDD
+ratingsRDD.take(6)  
+
+# Split fields using a map transformation
+SplittedRaingsRDD = ratingsRDD.map(lambda l : l.split('::'))
+
+# See what we've got now: 
+ratingsRDD.take(6)  
+
+# Create pairs M/R style for the counting task:
+RatingCountsRDD = SplittedRaingsRDD.map(lambda (uId, mId, r, ts) : (int(uId), 1))
+
+# Taking a sample of our partial counts
+Rsample = RatingCountsRDD.sample(False, 0.001)
+
+# See how big the sample is and inspect
+Rsample.count()
+Rsample.take(6) 
+
+
+
 
 
 
