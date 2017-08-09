f5ebcac1d7e11801b89b0591d3a7a040559386ee
Raul Arrabales <raul.arrabales@gmail.com>
2017-08-09 02:38:58 +0200

    Update Session4.py

--- Spark/Databricks/Session4.py
+++ Spark/Databricks/Session4.py
@@ -2,3 +2,15 @@
 # Databricks CE Cloud Practice
 # Raul Arrabales / Conscious-Robots.com 
 
+# Simple clustering example:
+from pyspark.mllib.clustering import KMeans, KMeansModel
+from numpy import array
+from math import sqrt 
+
+data = sc.textFile("data/kmeans_data.txt")
+
+# Load and parse input data
+data = sc.textFile("data/kmeans_data.txt")
+parsedData = data.map(lambda line: array([float(x) for x in line.split(' ')]))
+
+
