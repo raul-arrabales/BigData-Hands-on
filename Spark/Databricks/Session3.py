9a579d9c8fb7b3b08147bb714fea46b5f662cb31
Raul Arrabales <raul.arrabales@gmail.com>
2017-08-09 02:09:05 +0200

    Update Session3.py

--- Spark/Databricks/Session3.py
+++ Spark/Databricks/Session3.py
@@ -1,1 +1,21 @@
-faker
+# Spark Hands On Training
+# Databricks CE Cloud Practice
+# Raul Arrabales / Conscious-Robots.com 
+
+# Create a faker factory with a random seed
+from faker import Factory
+fake = Factory.create()
+fake.seed(6243)
+
+# Each entry consists of last_name, first_name, ssn, job, and age (at least 1)
+from pyspark.sql import Row
+def fake_entry():
+  name = fake.name().split()
+  return Row(name[1], name[0], fake.ssn(), fake.job(), abs(2016 - fake.date_time().year) + 1)
+
+# Create a helper function to call a function repeatedly
+def repeat(times, func, *args, **kwargs):
+    for _ in xrange(times):
+        yield func(*args, **kwargs)
+
+        
