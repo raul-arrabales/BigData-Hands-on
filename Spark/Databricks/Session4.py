00b3e749c2506b14a4542c861519a9c61ae081be
Raul Arrabales <raul.arrabales@gmail.com>
2017-08-09 19:29:00 +0200

    Update Session4.py

--- Spark/Databricks/Session4.py
+++ Spark/Databricks/Session4.py
@@ -4,6 +4,24 @@
 
 # See files in DBFS
 dbutils.fs.ls('/') 
+dbutils.fs.ls('/FileStore/tables')
+dbutils.fs.ls('/FileStore/tables/1x1xr57q1502297004187/') 
+
+# Also accesible as a table (if table declared during upload to CE)
+%sql select * from kmeans_table 
+
+# Applying KMeans (new ml lib - not mllib)
+from pyspark.ml.clustering import KMeans
+
+# Loading data: 
+# This is the old way, load into RDD
+# dataset = sc.textFile('/FileStore/tables/1x1xr57q1502297004187/kmeans_data.txt')
+
+# Reading CSV 
+kmeans_df = sqlContext.read.format("com.databricks.spark.csv") \
+  .option("header", "false").option("delimiter"," ") \
+  .load("/FileStore/tables/1x1xr57q1502297004187/kmeans_data.txt")
+  
 
 # Simple clustering example:
 from pyspark.mllib.clustering import KMeans, KMeansModel
