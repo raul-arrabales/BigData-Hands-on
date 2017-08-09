19bb5456897be17f3a3b3dfa1b219d7a9ef293ab
Raul Arrabales <raul.arrabales@gmail.com>
2017-08-10 01:44:25 +0200

    Update Session5.py

--- Spark/Databricks/Session5.py
+++ Spark/Databricks/Session5.py
@@ -2,3 +2,10 @@
 # Databricks CE Cloud Practice
 # Raul Arrabales / Conscious-Robots.com
 
+# JDBC Connection
+hostname = "yourDBserver.com"
+dbname = "yourDB"
+jdbcPort = 3306
+
+jdbcUrl = "jdbc:mysql://{0}:{1}/{2}?user={3}&password={4}".format(hostname, jdbcPort, dbname, username, password)
+
