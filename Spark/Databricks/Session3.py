3557e1bf5a60c2884b02f22004aa84edef5a5bdd
Raul Arrabales <raul.arrabales@gmail.com>
2017-08-09 02:12:48 +0200

    Update Session3.py

--- Spark/Databricks/Session3.py
+++ Spark/Databricks/Session3.py
@@ -11,11 +11,22 @@ fake.seed(6243)
 from pyspark.sql import Row
 def fake_entry():
   name = fake.name().split()
-  return Row(name[1], name[0], fake.ssn(), fake.job(), abs(2016 - fake.date_time().year) + 1)
+  return Row(name[1], name[0], fake.ssn(), fake.job(), abs(2017 - fake.date_time().year) + 1)
 
 # Create a helper function to call a function repeatedly
 def repeat(times, func, *args, **kwargs):
     for _ in xrange(times):
         yield func(*args, **kwargs)
 
-        
+# Create the dataset with 10k entries
+data = list(repeat(10000, fake_entry))
+
+# Check:
+data[0][0], data[0][1], data[0][2], data[0][3], data[0][4]
+len(data)
+
+# Build the DataFrame:
+dataDF = sqlContext.createDataFrame(data, ('last_name', 'first_name', 'ssn', 'occupation', 'age'))
+dataDF.printSchema()
+
+
