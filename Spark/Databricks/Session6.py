c51dab214df643d3d515f407e252826f3ebb9bff
Raul Arrabales <raul.arrabales@gmail.com>
2017-08-13 14:19:33 +0200

    Update Session6.py

--- Spark/Databricks/Session6.py
+++ Spark/Databricks/Session6.py
@@ -2,3 +2,11 @@
 # Databricks CE Cloud Practice
 # Raul Arrabales / Conscious-Robots.com
 
+# Example adapted from: 
+# Business Data Analysis by HiPIC of CalStateLA 
+# http://web.calstatela.edu/centers/hipic/ 
+
+# Data available at: 
+# https://raw.githubusercontent.com/hipic/biz_data_LA/master/Spark%20ML%20-%20Clustering/Business-Food.csv
+# Also at: 
+# 
