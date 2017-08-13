5f2595612461e232d5a8bef46b026cb4ed11401b
Raul Arrabales <raul.arrabales@gmail.com>
2017-08-13 14:24:22 +0200

    Update Session6.py

--- Spark/Databricks/Session6.py
+++ Spark/Databricks/Session6.py
@@ -2,11 +2,18 @@
 # Databricks CE Cloud Practice
 # Raul Arrabales / Conscious-Robots.com
 
+# Clustering of restaurants and food related businesses in LA area
+
 # Example adapted from: 
 # Business Data Analysis by HiPIC of CalStateLA 
 # http://web.calstatela.edu/centers/hipic/ 
 
-# Data available at: 
+# Data from Yelp available at: 
 # https://raw.githubusercontent.com/hipic/biz_data_LA/master/Spark%20ML%20-%20Clustering/Business-Food.csv
 # Also at: 
-# 
+# https://raw.githubusercontent.com/hipic/biz_data_LA/master/Spark%20ML%20-%20Clustering/Business-Food.csv 
+
+# Data is also stored in my DB Cloud Cluster at: 
+# /FileStore/tables/bwddhyjz1502626538261/Business_Food-4345d.csv 
+
+
