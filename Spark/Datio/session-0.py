82ec1996579fb608f5870b98df079b3f8face027
Raul Arrabales <raul.arrabales@gmail.com>
2017-10-24 14:02:50 +0200

    Update session-0.py

--- Spark/Datio/session-0.py
+++ Spark/Datio/session-0.py
@@ -11,4 +11,18 @@ sc.version
 rdd = sc.parallelize(range(1000))
 rdd.takeSample(False, 5)
 
+###########################
+# Quijote wc example
+
+# Getting the file:
+#
+# Copy&Paste is disabled
+# HTTPS as file system is disabled for TextFile
+# wget not installed (yum install wget)
+# wget https://raw.githubusercontent.com/raul-arrabales/BigData-Hands-on/master/data/quijote_complete.txt
+# and finally get the file in the local file system
+
+
+
+
 
