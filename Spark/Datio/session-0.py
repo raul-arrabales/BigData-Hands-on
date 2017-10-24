516249b0a7edbf338bb46d11aba024bbca34fc80
Raul Arrabales <raul.arrabales@gmail.com>
2017-10-24 13:38:32 +0200

    Update session-0.py

--- Spark/Datio/session-0.py
+++ Spark/Datio/session-0.py
@@ -3,9 +3,12 @@ import pyspark
 sc = pyspark.SparkContext('local[*]')
 
 # Check Spark Version
+sc
+
 sc.version
 
 # Do something to prove it works
 rdd = sc.parallelize(range(1000))
 rdd.takeSample(False, 5)
+
 
